e5131b9831aace1ad4967ececd5d6725af17ec67
Korry Cowher <KCowher14@yahoo.com>
2021-01-29 17:21:33 -0500

    finished gpu class and adjusted computer comp class

--- Computer-Build-Price-Estimator/src/main/java/com/techelevator/Computer_Build_Price_Estimator/ComputerComponent.java
+++ Computer-Build-Price-Estimator/src/main/java/com/techelevator/Computer_Build_Price_Estimator/ComputerComponent.java
@@ -1,34 +1,28 @@
 package com.techelevator.Computer_Build_Price_Estimator;
 
-public class ComputerComponent {
-	private String  type;
-	private String  tier;
+public abstract class ComputerComponent {
+	private int tier;
 	
-	public ComputerComponent(String type, String tier) {
-		this.type = type; 
-		this.tier = tier;
+	public ComputerComponent(String tier) { //Constructor asks for tier of part 
+		this.tier = Integer.parseInt(tier);
 	}
 	
-	public String getType() {
-		return this.type;
-	}
-	
-	public String getTier() {
+	public int getTier() { //allows user to access set tier
 		return this.tier;
 	}
 	
-	public long getPrice(long baseCost , String tier) {
-		long multiplier = 1;
-		long tierNumber = Long.parseLong(tier);
-		if(tierNumber == 2) {
-			multiplier = 2;
-		} else if ( tierNumber == 3) {
-			multiplier = 3;
-		}
-		return baseCost * multiplier;
-	}
+	public abstract String getType();
 	
+	public abstract double getPrice();
 	
-	
-	
+	//public long getPrice(long baseCost , String tier) { 
+	//	long multiplier = 1;
+	//	long tierNumber = Long.parseLong(tier);
+	//	if(tierNumber == 2) {
+	//		multiplier = 2;
+	//	} else if ( tierNumber == 3) {
+	//		multiplier = 3;
+	//	}
+	//	return baseCost * multiplier;
+	//  }	
 }
--- Computer-Build-Price-Estimator/src/main/java/com/techelevator/Computer_Build_Price_Estimator/Gpu.java
+++ Computer-Build-Price-Estimator/src/main/java/com/techelevator/Computer_Build_Price_Estimator/Gpu.java
@@ -1,5 +1,24 @@
 package com.techelevator.Computer_Build_Price_Estimator;
 
-public class Gpu {
+public class Gpu extends ComputerComponent {
+	private String type = "GPU";
+	private double basePrice = 200.00;
+	
+	public Gpu(String tier) {
+		super(tier);
+	}
 
+	@Override
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public double getPrice() {
+		if(getTier() == 2) {
+			return basePrice * 1.5;
+		} else if(getTier() == 3) {
+			return basePrice * 2.5;
+		} return basePrice;
+	}
 }
