98686e67e4b85a7116a4670bc51dcaa7dc6a589e
Korry Cowher <KCowher14@yahoo.com>
2021-02-02 21:29:52 -0500

    Started the application

---
+++ Computer-Build-Price-Estimator/src/main/java/com/techelevator/Computer_Build_Price_Estimator/Application.java
@@ -0,0 +1,28 @@
+package com.techelevator.Computer_Build_Price_Estimator;
+
+import java.util.Scanner;
+
+public class Application {
+	
+	public static void main(String[] args) {
+	
+		Scanner keyboardInput = new Scanner(System.in);
+	
+		System.out.println("Welcome to PLACEHOLDER Computer build estimator!\n\n\n"
+						+ "You will be given a choice of tier 1(base quality and least expensive) "
+						+ "to 3(premium component and most expensive)");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+}
+
+	
+}
